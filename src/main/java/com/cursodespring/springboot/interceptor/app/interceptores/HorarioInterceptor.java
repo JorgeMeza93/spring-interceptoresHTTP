c89808d2c145e22d74b16f0ce8ea26f36e02a63e
Jorge Meza Bello <jorberiver@gmail.com>
2022-11-16 13:20:44 -0600

    interceptor hecho

---
+++ src/main/java/com/cursodespring/springboot/interceptor/app/interceptores/HorarioInterceptor.java
@@ -0,0 +1,45 @@
+package com.cursodespring.springboot.interceptor.app.interceptores;
+
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class HorarioInterceptor implements HandlerInterceptor {
+	@Value("${config.horario.apertura}")
+	private Integer apertura;
+	@Value("${config.horario.cierre}")
+	private Integer cierre;
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		Calendar calendario = Calendar.getInstance();
+		int horaActual = calendario.get(Calendar.HOUR_OF_DAY);
+		if( horaActual >= apertura && horaActual< cierre){
+			StringBuilder mensaje = new StringBuilder("Bienvenido al servicio de atención a clientes");
+			mensaje.append(", atendemos desde las ");
+			mensaje.append(apertura);
+			mensaje.append("hrs. Hasta las ");
+			mensaje.append(cierre);
+			mensaje.append("hrs. Gracias por su visita"); 
+			request.setAttribute("mensaje", mensaje.toString());
+			return true;
+		}
+		response.sendRedirect(request.getContextPath().concat("/cerrado"));
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		String mensaje = (String)request.getAttribute("mensaje");
+		modelAndView.addObject("horario", mensaje);
+	}
+	
+}
